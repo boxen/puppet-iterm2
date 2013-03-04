a12fca5c8a92eb370553dc1984bba26316b4d6f0
Adam Ochonicki <adam@fromonesrc.com>
2013-03-04 10:41:52 -0500

    latest dev release

--- manifests/dev.pp
+++ manifests/dev.pp
@@ -1,6 +1,6 @@
 class iterm2::dev {
   package { 'iTerm':
-    source   => 'http://iterm2.googlecode.com/files/iTerm2-1_0_0_20120726.zip',
+    source   => 'http://iterm2.googlecode.com/files/iTerm2-1_0_0_20130302.zip',
     provider => 'compressed_app'
   }
 }
