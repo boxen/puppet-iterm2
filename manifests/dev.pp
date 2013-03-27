3edd276662734c1a1aba5b4fc85251cc20f6bbe1
Chris Denneen <cdenneen@gmail.com>
2013-03-26 23:11:40 -0400

    adding version to package name in dev

--- manifests/dev.pp
+++ manifests/dev.pp
@@ -1,5 +1,5 @@
 class iterm2::dev {
-  package { 'iTerm':
+  package { 'iTerm-dev-1_0_0_20130319':
     source   => 'https://iterm2.googlecode.com/files/iTerm2-1_0_0_20130319.zip',
     provider => 'compressed_app'
   }
