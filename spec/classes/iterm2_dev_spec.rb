88cd75659485fbf34a9bd83460e32ca3c25a9c48
Adam Ochonicki <adam@fromonesrc.com>
2013-05-25 13:35:00 -0400

    Indentation!

--- spec/classes/iterm2_dev_spec.rb
+++ spec/classes/iterm2_dev_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe 'iterm2::dev' do
   it do
     should contain_package('iTerm-dev-1_0_0_20130319').with({
-    :source   => 'http://iterm2.googlecode.com/files/iTerm2-1_0_0_20130319.zip',
-    :provider => 'compressed_app'
-  })
+      :source   => 'http://iterm2.googlecode.com/files/iTerm2-1_0_0_20130319.zip',
+      :provider => 'compressed_app'
+    })
   end
 end
--- spec/classes/iterm2_stable_spec.rb
+++ spec/classes/iterm2_stable_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe 'iterm2::stable' do
   it do
     should contain_package('iTerm').with({
-    :source   => 'http://iterm2.googlecode.com/files/iTerm2_v1_0_0.zip',
-    :provider => 'compressed_app'
-  })
+      :source   => 'http://iterm2.googlecode.com/files/iTerm2_v1_0_0.zip',
+      :provider => 'compressed_app'
+    })
   end
 end
 
