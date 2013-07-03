cf13d15753d5864fedee29530616a702d2eb1b5a
cdenneen <cdenneen@gmail.com>
2013-07-03 19:33:29 -0400

    Update stable.pp
    Updated new download location

--- manifests/stable.pp
+++ manifests/stable.pp
@@ -5,7 +5,7 @@
 #   include iterm2::stable
 class iterm2::stable {
   package { 'iTerm':
-    source   => 'http://iterm2.googlecode.com/files/iTerm2_v1_0_0.zip',
+    source   => 'http://www.iterm2.com/downloads/stable/iTerm2_v1_0_0.zip',
     provider => 'compressed_app'
   }
 }
