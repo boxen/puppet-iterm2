9ee8ccf23a96c18b210ebf30dce37095cb15ad7e
Carwin Young <carwinyoung@gmail.com>
2013-07-23 09:42:24 -0500

    Updated package to 1.0.0.20130624.

--- spec/classes/iterm2_dev_spec.rb
+++ spec/classes/iterm2_dev_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe 'iterm2::dev' do
   it do
-    should contain_package('iTerm-dev-1_0_0_20130319').with({
-      :source   => 'http://iterm2.googlecode.com/files/iTerm2-1_0_0_20130319.zip',
+    should contain_package('iTerm2-1_0_0_20130624').with({
+      :source   => 'http://www.iterm2.com/downloads/beta/iTerm2-1_0_0_20130624.zip',
       :provider => 'compressed_app'
     })
   end
