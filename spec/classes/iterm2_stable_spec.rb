a9494b31bf22ecdd5f22c5937852ce4431ef070a
cdenneen <cdenneen@gmail.com>
2013-11-09 22:44:43 -0500

    Update iterm2_stable_spec.rb

--- spec/classes/iterm2_stable_spec.rb
+++ spec/classes/iterm2_stable_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'iterm2::stable' do
   it do
     should contain_package('iTerm').with({
-      :source   => 'http://iterm2.googlecode.com/files/iTerm2_v1_0_0.zip',
+      :source   => 'http://www.iterm2.com/downloads/stable/iTerm2_v1_0_0.zip',
       :provider => 'compressed_app'
     })
   end
