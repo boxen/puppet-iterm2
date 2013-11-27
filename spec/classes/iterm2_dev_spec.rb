dacb0af63865427e597a13420f63ecfe8c88465a
razic <razic@viralkitty.com>
2013-11-26 18:37:07 -0800

    updates iterm2 dev from 1_0_0_20131116 to 1_0_0_20131124

--- spec/classes/iterm2_dev_spec.rb
+++ spec/classes/iterm2_dev_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe 'iterm2::dev' do
   it do
-    should contain_package('iTerm2-1_0_0_20131116').with({
-      :source   => 'http://www.iterm2.com/downloads/beta/iTerm2-1_0_0_20131116.zip',
+    should contain_package('iTerm2-1_0_0_20131124').with({
+      :source   => 'http://www.iterm2.com/downloads/beta/iTerm2-1_0_0_20131124.zip',
       :provider => 'compressed_app'
     })
   end
