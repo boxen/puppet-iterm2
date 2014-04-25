233d49ba5ea3c2f0143c9b7635aa49176e76c498
Mark Tareshawty <tarebyte@gmail.com>
2014-04-24 22:44:41 -0400

    update version of iterm2

--- manifests/dev.pp
+++ manifests/dev.pp
@@ -4,9 +4,9 @@
 #
 #   include iterm2::dev
 #   class { 'iterm2::dev':
-#     version => '20140112'
+#     version => '20140421'
 #   }
-class iterm2::dev($version = '20140403') {
+class iterm2::dev($version = '20140421') {
   package { "iTerm2-1_0_0_${version}":
     source   => "http://www.iterm2.com/downloads/beta/iTerm2-1_0_0_${version}.zip",
     provider => 'compressed_app'
--- spec/classes/iterm2_dev_spec.rb
+++ spec/classes/iterm2_dev_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'iterm2::dev' do
   it do
-    version = '20140403'
+    version = '20140421'
     should contain_package("iTerm2-1_0_0_#{version}").with({
       :source   => "http://www.iterm2.com/downloads/beta/iTerm2-1_0_0_#{version}.zip",
       :provider => 'compressed_app'
