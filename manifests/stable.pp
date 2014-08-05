a6f7a8b82649af52b966d1cdd1e63db89ff27cca
Romuald du Song <romuald.dusong@deveryware.com>
2014-08-05 09:14:18 +0200

    add version parameter

--- manifests/stable.pp
+++ manifests/stable.pp
@@ -3,9 +3,16 @@
 # Usage:
 #
 #   include iterm2::stable
-class iterm2::stable {
+#
+# or with specific version
+#
+#   class { 'iTerm':
+#     version => '2_0'
+#   }
+#
+class iterm2::stable($version='2_0') {
   package { 'iTerm':
-    source   => 'http://www.iterm2.com/downloads/stable/iTerm2_v2_0.zip',
+    source   => "http://www.iterm2.com/downloads/stable/iTerm2_v${version}.zip",
     provider => 'compressed_app'
   }
 }
