74afa6fe05a1e6ee02571f5b6b7a827d4c9362ff
Mark Tareshawty <tarebyte@gmail.com>
2014-10-07 12:02:46 -0400

    forgot ensure and updated spec

--- manifests/stable.pp
+++ manifests/stable.pp
@@ -15,6 +15,7 @@ class iterm2::stable (
   $version='2_0'
 ) {
   package { 'iTerm':
+    ensure  => $ensure,
     flavor   => 'zip',
     provider => 'compressed_app',
     source   => "http://www.iterm2.com/downloads/stable/iTerm2_v${version}.zip"
--- spec/classes/iterm2_stable_spec.rb
+++ spec/classes/iterm2_stable_spec.rb
@@ -4,8 +4,10 @@ describe 'iterm2::stable' do
   it do
     version = '2_0'
     should contain_package('iTerm').with({
-      :source   => "http://www.iterm2.com/downloads/stable/iTerm2_v#{version}.zip",
-      :provider => 'compressed_app'
+      :ensure   => 'present',
+      :flavor   => 'zip',
+      :provider => 'compressed_app',
+      :source   => "http://www.iterm2.com/downloads/stable/iTerm2_v#{version}.zip"
     })
   end
 end
