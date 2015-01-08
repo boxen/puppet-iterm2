ebaf747f5fffcab9bf00473fc6e0c1103fb4468e
Gerrard Geldenhuis <gerrard@puppetlabs.com>
2015-01-08 23:46:45 +0000

    Removing www from url in test

--- spec/classes/iterm2_dev_spec.rb
+++ spec/classes/iterm2_dev_spec.rb
@@ -4,7 +4,7 @@ describe 'iterm2::dev' do
   it do
     version = '20141103'
     should contain_package("iTerm").with({
-      :source   => "https://www.iterm2.com/downloads/beta/iTerm2-2_0_0_#{version}.zip",
+      :source   => "https://iterm2.com/downloads/beta/iTerm2-2_0_0_#{version}.zip",
       :provider => 'compressed_app'
     })
   end
